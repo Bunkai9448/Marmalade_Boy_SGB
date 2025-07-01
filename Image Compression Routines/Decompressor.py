eb2b1e2db3a745ca08b8cdd8991280a044ec96ef
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2025-07-01 22:08:56 +0200

    fix yuu offset

--- Image Compression Routines/Decompressor.py
+++ Image Compression Routines/Decompressor.py
@@ -71,7 +71,7 @@ rom_path = 'Marmalade Boy.gb'
 # List of offsets from which compressed data is taken
 offsets = [
     0x1803E,  # Meiko animation
-    0x189C8,  # Yuu animation - wrong offset
+    0x1880E,  # Yuu animation
     0x195C8,  # Arimi animation - wrong offset
     0x196E8,  # Ginta animation - wrong offset
     0x1C9FA,  # Ginta at the phone
