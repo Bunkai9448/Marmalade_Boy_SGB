af5154066f1080a19e57b8ba3dcff1449857d4bd
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2025-07-01 18:27:46 +0200

    adds tennis court offset again

--- Image Compression Routines/Decompressor.py
+++ Image Compression Routines/Decompressor.py
@@ -83,6 +83,7 @@ offsets = [
     0x257BB,  # Copyright screen
     0x25DE8,  # Password Robot
     0x27FE8,  # Highschool outside - review offset
+    0x2979B, # Tennis court
     0x31500,  # sprites for cookies and piano minigames - 1
     0x31700,  # sprites for cookies and piano minigames - 2
     0x31F11,  # sprites for coin minigame
