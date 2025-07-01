af161c772f46da6ce4d7452f9751f8281aa77653
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2025-07-01 21:06:55 +0200

    more offsets

--- Image Compression Routines/Decompressor.py
+++ Image Compression Routines/Decompressor.py
@@ -77,6 +77,7 @@ offsets = [
     0x1C9FA,  # Ginta at the phone
     0x1E7CC,  # Miki at the phone
     0x21B00,  # (END) psychology test results
+    0x222F4,  # (END) psychology test results - 2  - wrong offset
     0x2401A,  # Main (Start) Screen
     0x246C0,  # Triangles from intro screen
     0x25280,  # 銀太のテニスマッチ遊の　メダイュの秘密恋の行方
