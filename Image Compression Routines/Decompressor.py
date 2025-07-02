af60e62054cf1a42db697f84007f22d6f9544b00
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2025-07-02 20:47:14 +0200

    more offsets for graphic text

--- Image Compression Routines/Decompressor.py
+++ Image Compression Routines/Decompressor.py
@@ -75,6 +75,7 @@ offsets = [
     0x18F63,  # Arimi animation
     0x196DA,  # Ginta animation
     0x1A4E3,  # Miwa animation
+    0x1AFCE,  # 銀太との遊相性格　性診断のテスト
     0x1C9FA,  # Ginta at the phone
     0x1E7CC,  # Miki at the phone
     0x20DA1,  # One Point Advice
