579402e6f25775ed49c1ea84b4b65501fa359cf2
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2026-09-16 00:03:03 +0200

    inject ASM with python
    This is so i can inject all the ASM in order at once, keeping them as separated asm files for modularity.
    
    The calls from the script follow the repo estructure, but have a few subfolders renamed due to compatibility problems with python calls. Current ASM files from the repo are for standalone executing. If you run the python script without making a few changes with the input files names they won't be found, this will be updated later when to fix all crossreferences.
    
    This will also be updated with the image section, and the full text pointers stuff (inlcuding unifying of names for files and directories). For now is to keep things mostly organized, with the basics to be used by downloading the repo and doing a few quick changes, before I make a mess and lose it. 
    
    This is not the tester, but can be used as one.

---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import os
+import shutil
+
+from Text_Extraction_Insertion.Font_GFX import insert_bytes
+
+# This script as a whole being python should work in Windows and Unix Systems.
+# However, for the ASM patches it requires a different executable for each system
+# This little snippets is done to make that almost transparent for the user. Just need to set the variables accordingly.
+# A blank name "" will make the script jump over that command, this is done so you can re-run things separately for tests
+
+ROM="rom.gb"
+
+FONT="Text_Extraction_Insertion/Font.bin"         # The new Font Tiles
+EXP_ROUTINE=""  # The ROM expansion and header fixes (this is replicated in all ASM and has no single asm for it. Leaving here for completeness)
+DISP_ROUTINE="Text Display Routine\Working_Text_Hook_paired.asm"        # The new Font Size Display
+EXPAND_POINTERS="Text Display Routine\ExpandedROMnewScript.asm"     # The Pointers are now located in expanded ROM addresses
+
+IS_WINDOWS = True
+
+if IS_WINDOWS:
+    ARMIPS = "Text Display Routine/armips_at_gameboy.exe"
+else:
+    ARMIPS = "./Text Display Routine/armips_at_gameboy"
+
+def _create_backup():
+
+    if not os.path.exists("backup_rom.gb"):
+        shutil.copy2(ROM, "backup_rom.gb")
+
+def _run_command(command):
+    print("> " + " ".join(command)) # The tool will be verbose, so the user knows when the key sections are running.
+    result = subprocess.run(command)
+
+    if result.returncode != 0:
+        print(
+            f"ERROR: command failed with exit code "
+            f"{result.returncode}"
+        )
+        sys.exit(result.returncode)
+
+
+
+if __name__ == "__main__":
+
+# When Python is run as external command (instead of them as libs) so I can keep testing things separately without forgetting what was the name of each def.
+# You can change it to go one by one, to do it more cleanly.
+
+    _create_backup()
+
+    if FONT:
+        insert_bytes("backup_rom.gb", FONT, ROM, 0x33900)
+
+    if EXP_ROUTINE:
+        _run_command([ARMIPS, EXP_ROUTINE])
+
+    if DISP_ROUTINE:
+        _run_command([ARMIPS, DISP_ROUTINE])
+
+    if EXPAND_POINTERS:
+        _run_command([ARMIPS, EXPAND_POINTERS])
+
+
+    print("All ASM patches have been applied successfuly")
